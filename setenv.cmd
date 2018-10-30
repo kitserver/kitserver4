c96a202bb8bedbee7fd6982511ba565401278f99
Anton Jouline <anton.jouline@cbsinteractive.com>
2018-10-30 12:35:26 -0700

    updating setenv.cmd

--- setenv.cmd
+++ setenv.cmd
@@ -1,8 +1,9 @@
 @echo off
 echo Setting kitserver compile environment
-@call "c:\vs60\bin\vcvars32.bat"
+@call "c:\vs60ee\vc98\bin\vcvars32.bat"
 set DXSDK=c:\dxsdk81
-set INCLUDE=%DXSDK%\include;%INCLUDE%
-set LIB=%DXSDK%\lib;%LIB%
+set STLPORT=c:\stlport-4.6.2
+set INCLUDE=%DXSDK%\include;%STLPORT%\stlport;%INCLUDE%
+set LIB=%DXSDK%\lib;%STLPORT%\lib;%LIB%
 echo Environment set
 
